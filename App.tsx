536b838a018b545b55b61486d6efd9aa66fff558
Guilherme Vieira <Guizinho.vfs@gmail.com>
2021-11-10 12:00:32 -0300

    Adição de Stories por SCROLLBAR

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image,StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { estilo } from './css/Styles';
 
 export default function App() {
   return (
     <View style={estilo.container}>
 
+
+      {/* Inicio do header ####################################################*/}
       <View style={estilo.header}>
 
         <View style={estilo.perfil}>
-          <Image source={{ uri:"https://tntsports.com.br/__export/1630518367553/sites/esporteinterativo/img/2021/09/01/ronaldo_1.jpg_423682103.jpg"}} style={estilo.imgperfil} />
+          <Image source={{ uri: "https://tntsports.com.br/__export/1630518367553/sites/esporteinterativo/img/2021/09/01/ronaldo_1.jpg_423682103.jpg" }} style={estilo.imgperfil} />
         </View>
 
         <View style={estilo.titulo}>
@@ -19,10 +21,68 @@ export default function App() {
         </View>
 
         <View style={estilo.pesquisa}>
-          <Image source={{uri:"https://image.flaticon.com/icons/png/512/70/70866.png"}} style={estilo.imgpesquisa}/>
+          <Image source={{ uri: "https://cdn.pixabay.com/photo/2017/01/13/01/22/magnifying-glass-1976105_960_720.png" }} style={estilo.imgpesquisa} />
         </View>
+
       </View>
-      <StatusBar style="auto" />
+      {/* Fim do header ####################################################*/}
+
+
+
+
+      {/* Começo do Stories #################################################### */}
+      <View style={estilo.stories}>
+
+        <View>
+          <Text style={estilo.titulostories}> Stories </Text>
+        </View>
+
+        <ScrollView horizontal={true} style={estilo.scrollstories}>
+          <View>
+            <Text style={estilo.blackbox}>Live</Text>
+            <Image source={{ uri: "https://s1.1zoom.me/big0/389/298333-svetik.jpg" }} style={estilo.imgstories} ></Image>
+            <View style={estilo.txticon}>
+              <Text style={estilo.txt}>Savannah</Text>
+              <Image source={require("./assets/iconverified.png")} style={estilo.icon}></Image>
+            </View>
+          </View>
+
+          <View>
+            <Text style={estilo.redbox}>Premiere</Text>
+            <Image source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdvq7kRZc3dJCOWIklmoyNEu1yHhUm5zXNkQ&usqp=CAU" }} style={estilo.imgstories} ></Image>
+
+          </View>
+
+          <View>
+            <Text style={estilo.blackbox}>Live</Text>
+            <Image source={{ uri: "https://s1.1zoom.me/big0/931/Volkswagen_Baja_1000_Beetles_2017_534019_1280x800.jpg" }} style={estilo.imgstories} ></Image>
+            <View style={estilo.txticon}>
+              <Text style={estilo.txt}>Savannah</Text>
+              <Image source={require("./assets/iconverified.png")} style={estilo.icon}></Image>
+            </View>
+          </View>
+          <View>
+            <Text style={estilo.redbox}>Premiere</Text>
+            <Image source={{ uri: "https://s1.1zoom.me/big0/284/353588-admin.jpg" }} style={estilo.imgstories} ></Image>
+            <View style={estilo.txticon}>
+              <Text style={estilo.txt}>Savannah</Text>
+              <Image source={require("./assets/iconverified.png")} style={estilo.icon}></Image>
+            </View>
+          </View>
+
+          <View>
+            <Text style={estilo.blackbox}>Live</Text>
+            <Image source={{ uri: "https://s1.1zoom.me/big0/380/McLaren_Yellow_504332.jpg" }} style={estilo.imgstories} ></Image>
+            <View style={estilo.txticon}>
+              <Text style={estilo.txt}>Savannah</Text>
+              <Image source={require("./assets/iconverified.png")} style={estilo.icon}></Image>
+            </View>
+          </View>
+        </ScrollView>
+
+      </View>
+      {/* Fim do Stories #################################################### */}
+
 
     </View>
   );
--- css/Styles.tsx
+++ css/Styles.tsx
@@ -1,54 +1,118 @@
 import { StyleSheet } from "react-native"
 
-export const estilo= StyleSheet.create({
-    container:{
-    backgroundColor:'#090909',
-    flex:1,
-    paddingTop:50
-    },
-    header:{
-        width:'100%',
-        flexDirection:'row',
-        display:'flex',
-        paddingLeft:'10',
-        paddingRight:'10',
-        paddingBottom:'30'
-    },
-    perfil:{
-
-    },
-    imgperfil:{
-        width:110,
-        height:100,
-        resizeMode:'contain',
-        borderRadius:800
-    },
-    titulo:{
-        flex:2
-    },
-    nome:{
-        color:'#989CAC',
-        textAlign:'left',
-        fontSize:30
-    },
-    descricao:{
-        color:'#989CAC',
-        textAlign:'left',
-        fontSize:15
-    },
-    pesquisa:{
-        width:55,
-        height:50,
-        backgroundColor:'#989CAC',
-        borderRadius:800
-    },
-    imgpesquisa:{
-        flex:1,
-        width:40,
-        height:40,
-        resizeMode:'contain',
-        alignContent:'center',
-        paddingLeft:'50%'
+export const estilo = StyleSheet.create({
+    //Container
+    container: {
+        backgroundColor: '#090909',
+        flex: 1,
+        paddingTop: 50
     },
 
+    //Começo do Header ############################
+    header: {
+        width: '100%',
+        flexDirection: 'row',
+        display: 'flex',
+        paddingLeft: '10',
+        paddingRight: '10',
+        paddingBottom: '30'
+    },
+    perfil: {
+        paddingLeft: 10
+    },
+    imgperfil: {
+        width: 110,
+        height: 100,
+        resizeMode: 'contain',
+        borderRadius: 800
+    },
+    titulo: {
+        flex: 2,
+        paddingLeft: '10'
+    },
+    nome: {
+        color: '#989CAC',
+        textAlign: 'left',
+        fontSize: 30,
+        paddingLeft: 10
+    },
+    descricao: {
+        color: '#989CAC',
+        textAlign: 'left',
+        fontSize: 15,
+        paddingLeft: 10
+    },
+    pesquisa: {
+        paddingRight: 10
+    },
+    imgpesquisa: {
+        flex: 1,
+        width: 40,
+        height: 40,
+        resizeMode: 'contain',
+        alignContent: 'center',
+
+    },
+    //Fim do Header #################################################
+    stories: {
+
+    },
+    titulostories: {
+        color: '#989CAC',
+        fontSize: 20,
+        margin: 10
+    },
+    scrollstories: {
+
+    },
+    imgstories: {
+        width: 200,
+        height: 300,
+        resizeMode: 'contain',
+        borderRadius: 60,
+        margin: 10
+    },
+    blackbox: {
+        width: 80,
+        height: 40,
+        borderRadius: 10,
+        backgroundColor: '#090909',
+        color: '#989CAC',
+        fontSize: 15,
+        top: 20,
+        left: 20,
+        position: 'absolute',
+        zIndex: 1000,
+        padding: 10
+
+    },
+    redbox: {
+        width: 80,
+        height: 40,
+        borderRadius: 10,
+        backgroundColor: '#600',
+        color: '#989CAC',
+        fontSize: 15,
+        top: 20,
+        left: 20,
+        position: 'absolute',
+        zIndex: 1000,
+        padding: 10
+
+    },
+    txticon: {
+        flexDirection: 'row'
+    },
+    txt: {
+        color: '#989CAC',
+        padding: 20,
+        fontSize: 10
+    },
+    icon: {
+        width: 18,
+        height: 18,
+        resizeMode: 'contain'
+    }
+
+    //Comeco do Stories
 })
